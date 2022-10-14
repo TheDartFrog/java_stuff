0e0cf086108c37019c1450bdec62e65f5ecf0bf8
TheDartFrog <30552512+TheDartFrog@users.noreply.github.com>
2022-10-14 17:18:09 +0200

    e

---
+++ 01E/one.java
@@ -0,0 +1,52 @@
+import java.util.*;  
+import java.nio.charset.Charset;
+
+
+
+
+public class one {
+
+	
+	public static void main(String[] args)
+	{
+		
+		Random rand = new Random();
+		
+		int minRange = 2137, maxRange= 69000;
+        int value = rand.nextInt(maxRange - minRange) + minRange;
+		System.out.println("\nWELCOME!!\n\n[1]\n\nGenerating random number...");
+		System.out.println(value);
+		
+		System.out.println("\nConverting to binary...");
+		
+		System.out.println(Integer.toBinaryString(value));
+		
+		System.out.println("\nConverting to octal...");
+		
+		System.out.println(Integer.toOctalString(value));
+		
+		System.out.println("\nConverting to hexadecimal...");
+		
+		System.out.println(Integer.toHexString(value));
+		
+		System.out.println("\nDONE..");
+		
+		System.out.println("\n[2]");
+		
+		char aaaaaa = 'A';
+		
+		System.out.println(aaaaaa+0);
+		
+		System.out.println("\n[3]\n\n");
+		
+		
+		System.out.println(System.getProperty("file.encoding"));
+		
+		System.out.print('Ż');
+		System.out.print('ó');
+		System.out.print('ł');
+		System.out.print('ć');
+	}
+
+
+}
---
+++ 1stThingy/Hello.java
@@ -0,0 +1,67 @@
+
+
+public class Hello {
+	public static void main(String[] args)
+	
+	{
+		
+		System.out.println("among us");
+		System.out.print("sussy"); //bruhhhhhhhhhhhh
+		/* 
+		yelloww
+		why
+		*/
+		
+		//EASY on/off comment switch!
+		
+		/**
+		System.out.println("bruhh");
+		/**/
+		
+		// adding a "/" changes it! :D
+		
+		/**/
+		System.out.println("bruhh");
+		/**/
+		
+		int i = 1;
+		int o = 0;
+		boolean hasReached;
+		if (hasReached = true)
+		{
+		while (i<16)
+			{
+			
+				System.out.print("Bruh");
+				while(o<i)
+				{
+					System.out.print("h");
+					o++;
+				}
+				i++;
+				o = 0;
+				System.out.println();
+			
+			}
+			hasReached = true;
+		} else {
+		
+		while (i<0)
+			{
+			
+				System.out.print("Bruh");
+				while(o<i)
+				{
+					System.out.print("h");
+					o++;
+				}
+				i++;
+				o = 0;
+				System.out.println();
+			
+			}
+		
+		}
+	}
+	
+}
